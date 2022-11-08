def38a39409975c98aa7976991f6a8d95dca129d
himathaivalappil <himathaivalappil@gmail.com>
2022-11-08 14:40:21 +0400

    data validation config

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -2,28 +2,45 @@ from datetime import datetime
 import os
 from sensor.constant.training_pipeline import *
 
-class TrainingPipelineConfig():
-    
-    def __init__(self,timestamp=datetime.now()):
+
+class TrainingPipelineConfig:
+    def __init__(self, timestamp=datetime.now()):
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name = PIPELINE_NAME
         self.artifact = os.path.join(ARTIFACT_DIR, timestamp)
         self.timestamp = timestamp
-class DataIngestionConfig():
 
-    def __init__(self, training_pipeline_config = TrainingPipelineConfig()):
+
+class DataIngestionConfig:
+    def __init__(self, training_pipeline_config=TrainingPipelineConfig()):
         self.data_ingestion_dir = os.path.join(
-            training_pipeline_config.artifact, DATA_INGESTION_DIR_NAME)
-
+            training_pipeline_config.artifact, DATA_INGESTION_DIR_NAME
+        )
         self.feature_store_filepath = os.path.join(
-            self.data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
-
+            self.data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME
+        )
         self.training_filepath = os.path.join(
-            self.data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+            self.data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME
+        )
         self.testing_filepath = os.path.join(
-            self.data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+            self.data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME
+        )
         self.train_test_split_ration = DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
         self.collection_name = DATA_INGESTION_COLLECTION_NAME
 
 
-    
+class DataValidationConfig:
+    def __init__(self, training_pipeline_config=TrainingPipelineConfig()):
+        self.data_validation_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME
+        )
+        self.valid_data_dir: str = os.path.join(
+            self.data_validation_dir, DATA_VALIDATION_VALID_DIR
+        )
+        self.invalid_data_dir: str = os.path.join(
+            self.data_validation_dir, DATA_VALIDATION_INVALID_DIR
+        )
+        self.valid_train_file_path: str = os.path.join(self.valid_data_dir, TRAIN_FILE_NAME)
+        self.valid_test_file_path: str = os.path.join(self.valid_data_dir, TEST_FILE_NAME)
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, TRAIN_FILE_NAME)
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, TEST_FILE_NAME)
