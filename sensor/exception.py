a0770016a0faed0537669d53ef1b913543c2b85c
himathaivalappil <himathaivalappil@gmail.com>
2022-11-10 13:44:52 +0400

    exception and logger changes

--- sensor/exception.py
+++ sensor/exception.py
@@ -1,25 +1,27 @@
-import sys 
+import sys
 
-def error_message_detail(error_message, error_detail):
+
+def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
 
     file_name = exc_tb.tb_frame.f_code.co_filename
-    full_error_message = "Error occured in python script name [{0}], line number [{1}], error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, error_message)
 
-    return full_error_message
+    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+
+    return error_message
+
 
 class SensorException(Exception):
-    def __init__(self, error_message, error_detail):
-        """_summary_
-
-        Args:
-            error_message (string): Error message generated
-            error_detail (sys): sys variable containing 
+    def __init__(self, error_message, error_detail:sys):
         """
+        :param error_message: error message in string format
+        """
+        super().__init__(error_message)
 
         self.error_message = error_message_detail(
-            error_message, error_detail
+            error_message, error_detail=error_detail
         )
 
     def __str__(self):
--- sensor/logger.py
+++ sensor/logger.py
@@ -1,18 +1,18 @@
 import logging
 import os
-import datetime
-from from_root import from_root
+from datetime import datetime
+import os
 
-LOG_FILE = f"{datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(from_root(), "logs", LOG_FILE)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
 os.makedirs(logs_path, exist_ok=True)
+
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-    level = logging.INFO,
-    format="[%(asctime)s] - %(lineno)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler(LOG_FILE_PATH, mode='w'),
-        logging.StreamHandler()
-    ]
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
