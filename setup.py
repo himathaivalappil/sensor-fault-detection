86948d6655f4618e113323b8e4038d2a4adbf49a
himathaivalappil <himathaivalappil@gmail.com>
2022-11-10 13:38:15 +0400

    main and setup

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
 def get_requirements()->List[str]:
     """
-    This fn will return the list of requiremnets
+    This function will return list of requirements
     """
-    requirement_list:List[str]= []
+    requirement_list:List[str] = []
 
-    # Write fn to read from requiremnets.txt file and convert into a list.  
+    """
+    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
+    """
     return requirement_list
 
 setup(
     name="sensor",
     version="0.0.1",
-    author="Hima",
-    author_email="himathaivalappil@gmail.com",
-    packages=find_packages(),
-    install_requires=get_requirements()     #["pymongo==4.2.0"],
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
+    packages = find_packages(),
+    install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
 
-# to run python setup.py install
